9338c46e27a1cb7ed33c3de8e769c9927dcdaca2
cipry80 <cipry80@gmail.com>
2015-02-25 22:10:22 +0200

    rename poze1

---
+++ db_data.php
@@ -0,0 +1,14 @@
+<?php
+$array_db = array (
+    1 => array("nume" => "I see in two?", "url" => "www.cipry.dev/public/images/poza1.jpg"),
+    2 => array("nume" => "You can call me sir!!!! ", "url" => "www.cipry.dev/public/images/poza2.jpg"),
+    3 => array("nume" => "Stop, right there.", "url" => "www.cipry.dev/public/images/poza3.jpg"),
+    4 => array("nume" => "Let's play football.", "url" => "www.cipry.dev/public/images/poza4.jpg"),
+    5 => array("nume" => "Je te aime", "url" => "www.cipry.dev/public/images/poza5.jpg"),
+    6 => array("nume" => "Alone, in grass.", "url" => "www.cipry.dev/public/images/poza6.jpg"),
+    7 => array("nume" => "Red eyes.", "url" => "www.cipry.dev/public/images/poza7.jpg"),
+    8 => array("nume" => "Party, yup :)", "url" => "www.cipry.dev/public/images/poza8.jpg"),
+    9 => array("nume" => "I will sunk.", "url" => "www.cipry.dev/public/images/poza9.jpg"),
+    10 => array("nume" => "Reduce fat.", "url" => "www.cipry.dev/public/images/poza10.jpg")
+);
+
