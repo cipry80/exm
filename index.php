e928a4174abc4c9c72284d796ab6b15bd798e7fe
cipry80 <cipry80@gmail.com>
2015-02-25 23:34:10 +0200

    update

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
 require_once 'config/db.php';
-include 'db_data.php';
-<html>
+include 'db_data.php';?>
+<html> 
     <head> 
     </head>
     <body>
         <div id="main">
-            <table id="stanga"></table>
+            <table id="stanga"><?php 
+        foreach ($array_db as $id => $row) {
+            echo 'Nume: '. $row['nume'];
+            echo 'Url: ' . $row['url'];
+            }?></table>
+            
             <table id="dreapta"></table>
         </div>
     </body>
