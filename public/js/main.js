b369ef4099251c9259bb7adf5cfcf036411e6853
cipry80 <cipry80@gmail.com>
2015-03-04 21:57:33 +0200

    update 04.03

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,25 @@
 $( function() {
     $.getJSON('http://www.ciprian.dev/arrayEchoData.php', function(data){
         $.each(data, function(key, val){
-            $('#stanga').append('<tr>' + '<td>'+ val.nume + '</td>' + '<td>'+ val.url + '</td>');
+            $('#stanga').append('<tr class="name">' + '<td>'+ val.nume + '</td>' + '<td class="description">'+ val.url + '</td>');
         });
         $('td').click( function(){
 	$(this).toggleClass('red-cell');
             var clickUrl = $(this).hasClass( 'name' );
-            console.log(clickUrl);
             var clickDescriere = $(this).hasClass( 'description' );
-            console.log(clickDescriere);
+            if(clickUrl === true){
+                if(clicDescriere === true){
+                    
+                            
+                            
+                }
+                else{
+                    //wrong select
+                }
+            }
+            else{
+                    //wrong select
+                }
            
         });
     });
